1c3af196d3d5673e34fb4ab37e3cf97562e4f488
Igor <eternity3648@gmail.com>
2020-07-28 00:08:37 +0300

    Made cube drop

---
+++ Assets/GameScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScreen : MonoBehaviour
+{
+    public GameObject cube;
+
+    private Vector3 cubePosition = new Vector3(0, 0, 0);
+    private int cubeSpeed = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cubePosition = new Vector3(0, cubePosition.y - cubeSpeed * Time.deltaTime, 0);
+        cube.transform.position = cubePosition;
+    }
+}
